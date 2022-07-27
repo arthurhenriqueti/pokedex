b4c65ad349c74a32a39f6eb5a39c036c2b2f3624
Arthur Henrique <arthurbleckrep2@gmail.com>
2022-07-27 08:45:03 -0300

    requisition created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
+import { useState, useEffect } from "react";
+import { api } from "../services/api";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "./Home.scss";
 
 const Home = () => {
+    const [pokemons, setPokemons] = useState([]);
+
+    const getPokemons = async () => {
+        let newArray = [];
+        for (let i = 1; i <= 20; i++) {
+            await api.get(`${i}`).then((response) => {
+                return (newArray = [...newArray, response.data]);
+            });
+        }
+        setPokemons[newArray];
+    };
+
+    useEffect(() => {
+        getPokemons();
+    }, []);
+
     return (
         <>
             <Header />
             <main className="home-background">
                 <h1>Página Home</h1>
+                <div className="home-pokemons-area container">
+                    {pokemons?.map((pokemon, index) => (
+                        <p>{pokemon.name}</p>
+                    ))}
+                </div>
             </main>
             <Footer />
         </>
